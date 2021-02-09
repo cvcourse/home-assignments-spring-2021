4efea20b4c101e7054f48275b83b1cce4402dee5
Daria Razumova <d.razumova@yandex.ru>
2021-02-09 20:56:39 +0300

    hw1 done

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -46,17 +46,60 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+def _merge_corners(corner_points, new_corner_points, maxCorners):
+    new_corner_points = np.array([i for i in new_corner_points if i not in corner_points]) # todo
+
+    corner_points = np.concatenate((corner_points, new_corner_points), axis=0)
+    
+    np.random.shuffle(corner_points)
+    return corner_points[:maxCorners]
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    
     image_0 = frame_sequence[0]
+
+    maxCorners = image_0.shape[0] * image_0.shape[1] // 1000
+    qualityLevel = 0.01
+    minDistance = 10
+    blockSize = 7
+
+    corner_points = cv2.goodFeaturesToTrack(image_0, maxCorners, qualityLevel, minDistance, blockSize = blockSize)
+    ids = np.arange(0, corner_points.shape[0], 1).reshape((-1, 1))
+    sizes = np.full((corner_points.shape[0], 2), blockSize)
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        ids,
+        corner_points,
+        sizes
     )
     builder.set_corners_at_frame(0, corners)
+
+    maxLevel = 3
+    maxCount = 10
+    epsilon = 0.03
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, maxCount, epsilon)
+
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        image_0_8u = (image_0 * 255).astype(np.uint8) 
+        image_1_8u = (image_1 * 255).astype(np.uint8) 
+    
+        nextPts, status, err = cv2.calcOpticalFlowPyrLK(image_0_8u, image_1_8u, corner_points, None,
+         winSize = (blockSize, blockSize), maxLevel = maxLevel, criteria = criteria)
+
+        corner_points = nextPts[status == 1]
+        
+        new_corner_points = cv2.goodFeaturesToTrack(image_1, maxCorners, qualityLevel, minDistance, blockSize = blockSize)
+        corner_points = _merge_corners(nextPts, new_corner_points, maxCorners)
+        ids = np.arange(0, corner_points.shape[0], 1).reshape((-1, 1)) # todo
+        sizes = np.full((corner_points.shape[0], 2), blockSize)
+        
+        corners = FrameCorners(
+            ids,
+            corner_points,
+            sizes
+        )
+
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
